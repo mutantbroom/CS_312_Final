e65f7f87a09e29eda0d5cfa06f8bffb166bf7e9b
mutantbroom <jon.beck@live.com>
2023-06-14 18:57:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,4 +11,10 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+
+variable "mojang_server_url" {
+  type    = string
+  default = "https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar"
 }
